3d37f23564d8c6ebfebeb48e7db3592896d28d03
KeshiEmmanuel <keshichidera@gmail.com>
2025-10-08 20:31:32 -0400

    refactor: overhaul application structure and remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import About from "./sections/About";
-import Hero from "./sections/Hero";
-import Navbar from "./components/Navbar";
-import Projects from "./sections/Projects";
-import { Contact } from "./sections/Contact";
+import { FaXTwitter, FaLinkedin } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
 
-export default function App() {
+export const App = () => {
     return (
-        <div className="max-container">
-            <Navbar />
-            <Hero />
-            <About />
-            <Projects />
-            <Contact />
-        </div>
+        <main className="max-w-[600px] mx-auto py-20 font-primary">
+            <h1 className="text-xl font-semibold">
+                Hi, I'm Chukwuebuka Oluwatobi
+            </h1>
+            <p className="text-sm text-gray-700 py-1">
+                17yo Software Engineer with passion for building apps.
+            </p>
+            <div className="flex items-center py-1 gap-2">
+                <a
+                    target="_blank"
+                    className="hover:opacity-75 transition-opacity"
+                    href="https://x.com/001Solopreneur"
+                >
+                    <FaXTwitter />
+                </a>
+                <a
+                    target="_blank"
+                    href="https://www.linkedin.com/in/keshiemmanuel/"
+                    className="hover:opacity-75 transition-opacity"
+                >
+                    <FaLinkedin />
+                </a>
+                <a
+                    target="_blank"
+                    href="mailto:keshichukwuebuka@gmail.com"
+                    className="hover:opacity-75 transition-opacity"
+                >
+                    <IoMdMail />
+                </a>
+            </div>
+        </main>
     );
-}
+};
