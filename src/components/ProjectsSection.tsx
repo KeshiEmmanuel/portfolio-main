858253303967e6b2b79bd2dfe76f0c2f3f348a51
KeshiEmmanuel <keshichidera@gmail.com>
2026-06-07 00:10:33 +0100

    removed import "react"

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion, useAnimationFrame, useMotionValue } from "framer-motion";
 
 // ─── Types ────────────────────────────────────────────────────────────────────
